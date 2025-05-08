f3ab85d46a0414831103bff197c36ca80f06c54b
sozcelyk43 <sozcelyk@gmail.com>
2025-05-09 01:55:48 +0300

    Update settings.py

--- sinav_sistemi/settings.py
+++ sinav_sistemi/settings.py
@@ -124,3 +124,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# SinavProjesi/sinav_sistemi/settings.py dosyasının sonuna
+
+LOGIN_REDIRECT_URL = '/'  # Başarılı girişten sonra yönlendirilecek varsayılan URL (ana sayfa)
+LOGOUT_REDIRECT_URL = '/' # Başarılı çıkıştan sonra yönlendirilecek varsayılan URL (ana sayfa)
+LOGIN_URL = '/hesap/login/' # Giriş yapılmamışsa yönlendirilecek URL
+
+
+
