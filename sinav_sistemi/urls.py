c86b9173fd7fc2cf100d6b00a21621c9f00033c9
sozcelyk43 <sozcelyk@gmail.com>
2025-05-09 01:52:05 +0300

    Update urls.py

--- sinav_sistemi/urls.py
+++ sinav_sistemi/urls.py
@@ -1,10 +1,11 @@
 
 from django.contrib import admin
-from django.urls import path, include
-from sonuclar import views as sonuclar_views # sonuclar uygulamasının views.py dosyasını import ediyoruz
+from django.urls import path, include 
+from sonuclar import views as sonuclar_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sonuclar/', include('sonuclar.urls')), # sonuclar uygulamasının kendi URL'lerini dahil et
-    path('', sonuclar_views.ana_sayfa_view, name='ana_sayfa'), # KÖK URL İÇİN BU SATIRIN OLDUĞUNDAN EMİN OLUN
+    path('hesap/', include('django.contrib.auth.urls')),
+    path('sonuclar/', include('sonuclar.urls')),
+    path('', sonuclar_views.ana_sayfa_view, name='ana_sayfa'),
 ]
