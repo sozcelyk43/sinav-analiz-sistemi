2ba2b390375cab52228e99af978af5de8f60bef2
sozcelyk43 <sozcelyk@gmail.com>
2025-05-22 02:57:31 +0300

    Update settings.py

--- sinav_sistemi/settings.py
+++ sinav_sistemi/settings.py
@@ -88,7 +88,8 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_live')
+STATICFILES_DIRS = [ BASE_DIR / "static", ]
+STATIC_ROOT = BASE_DIR / "staticfiles_live" 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
